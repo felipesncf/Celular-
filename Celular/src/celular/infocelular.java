1bf86bd305a341a45676ec9830638f99f60ceefe
felipesncf <47672065+felipesncf@users.noreply.github.com>
2019-03-19 16:13:38 -0300

    Add files via upload

---
+++ Celular/src/celular/infocelular.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package celular;
+
+/**
+ *
+ * @author pioca_000
+ */
+public class infocelular {
+    private int preco;
+    private String modelo;
+    private String marca;
+    private int memoria;
+
+    /**
+     * @return the preco
+     */
+    public int getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @return the memoria
+     */
+    public int getMemoria() {
+        return memoria;
+    }
+
+    /**
+     * @param memoria the memoria to set
+     */
+    public void setMemoria(int memoria) {
+        this.memoria = memoria;
+    }
+    
+    
+    
+}
